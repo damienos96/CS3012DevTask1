805fe3d7ce93a009f692153d6e4c125b39666eaa
damienos96 <damiandebny@hotmail.com>
2017-10-20 23:18:46 +0100

    Test height() and isEmpty()

--- src/DevTask1Test.java
+++ src/DevTask1Test.java
@@ -19,97 +19,76 @@ public class DevTask1Test {
 	public void testHeight()
 	{
 		DevTask1<Integer, Integer> bst = new DevTask1<Integer, Integer>();
+		assertEquals("Checking the height() function for an empty tree", -1, bst.height());
 		bst.put(7, 7);
 	    bst.put(8, 8);
 	    bst.put(3, 3);
-	    bst.put(1, 1);
-	    bst.put(2, 2);
-	    bst.put(6, 6);
-	    bst.put(4, 4);
-	    bst.put(5, 5);
+	    assertEquals("Checking the height() function for a non-empty tree", 1, bst.height());
 	}
 	
 	@Test
 	public void testIsEmpty()
 	{
 		DevTask1<Integer, Integer> bst = new DevTask1<Integer, Integer>();
+		assertEquals("Checking the isEmpty() function for an empty tree", true, bst.isEmpty());
 		bst.put(7, 7);
 	    bst.put(8, 8);
 	    bst.put(3, 3);
-	    bst.put(1, 1);
-	    bst.put(2, 2);
-	    bst.put(6, 6);
-	    bst.put(4, 4);
-	    bst.put(5, 5);
+	    assertEquals("Checking the isEmpty() function for a non-empty tree", false, bst.isEmpty());
 	}
 	
 	@Test
 	public void testSize()
 	{
 		DevTask1<Integer, Integer> bst = new DevTask1<Integer, Integer>();
+		
 		bst.put(7, 7);
 	    bst.put(8, 8);
 	    bst.put(3, 3);
-	    bst.put(1, 1);
-	    bst.put(2, 2);
-	    bst.put(6, 6);
-	    bst.put(4, 4);
-	    bst.put(5, 5);
+	    
 	}
 	
 	@Test
 	public void testContains()
 	{
 		DevTask1<Integer, Integer> bst = new DevTask1<Integer, Integer>();
+		
 		bst.put(7, 7);
 	    bst.put(8, 8);
 	    bst.put(3, 3);
-	    bst.put(1, 1);
-	    bst.put(2, 2);
-	    bst.put(6, 6);
-	    bst.put(4, 4);
-	    bst.put(5, 5);
+	    
 	}
 	
 	@Test
 	public void testDelete()
 	{
 		DevTask1<Integer, Integer> bst = new DevTask1<Integer, Integer>();
+		
 		bst.put(7, 7);
 	    bst.put(8, 8);
 	    bst.put(3, 3);
-	    bst.put(1, 1);
-	    bst.put(2, 2);
-	    bst.put(6, 6);
-	    bst.put(4, 4);
-	    bst.put(5, 5);
+	    
 	}
 	
 	@Test
 	public void testFloor()
 	{
 		DevTask1<Integer, Integer> bst = new DevTask1<Integer, Integer>();
+		
 		bst.put(7, 7);
 	    bst.put(8, 8);
 	    bst.put(3, 3);
-	    bst.put(1, 1);
-	    bst.put(2, 2);
-	    bst.put(6, 6);
-	    bst.put(4, 4);
-	    bst.put(5, 5);
+	    
 	}
 	
 	@Test
 	public void testLowestCommonAncestor()
 	{
 		DevTask1<Integer, Integer> bst = new DevTask1<Integer, Integer>();
+		
 		bst.put(7, 7);
 	    bst.put(8, 8);
 	    bst.put(3, 3);
-	    bst.put(1, 1);
-	    bst.put(2, 2);
-	    bst.put(6, 6);
-	    bst.put(4, 4);
-	    bst.put(5, 5);
+	    
 	}
 }
