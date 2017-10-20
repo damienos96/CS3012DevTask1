508caf356e01350bb08970931c4bab66e948035e
damienos96 <damiandebny@hotmail.com>
2017-10-20 20:10:41 +0100

    Node class

--- src/DevTask1.java
+++ src/DevTask1.java
@@ -2,5 +2,21 @@
 import java.util.NoSuchElementException;
 
 public class DevTask1<Key extends Comparable<Key>, Value> {
-    
+	private Node root;             // root of BST
+
+    /**
+     * Private node class.
+     */
+    private class Node {
+        private Key key;           // sorted by key
+        private Value val;         // associated data
+        private Node left, right;  // left and right subtrees
+        private int N;             // number of nodes in subtree
+
+        public Node(Key key, Value val, int N) {
+            this.key = key;
+            this.val = val;
+            this.N = N;
+        }
+    }
 }
