d2c62525191ab91edac8509a8e65c6a91ad0e6ab
damienos96 <damiandebny@hotmail.com>
2017-10-20 20:25:03 +0100

    Lowest common ancestor function

--- src/DevTask1.java
+++ src/DevTask1.java
@@ -178,4 +178,35 @@ public class DevTask1<Key extends Comparable<Key>, Value> {
     	return null;
     }
     
+    public Node lowestCommonAncestor(Node root, Node p, Node q) {
+        if(root==null)
+        {
+        	return null;
+        }
+        if(root==p || root==q)
+        {
+        	return root;
+        }
+        Node l = lowestCommonAncestor(root.left, p, q);
+        Node r = lowestCommonAncestor(root.right, p, q);
+        if(l != null && r != null)
+        {
+            return root;
+        }
+        else if(l == null && r == null)
+        {
+            return null;
+        }
+        else
+        {
+        	if(l == null)
+        	{
+        		return r;
+        	}
+        	else
+        	{
+        		return l;
+        	}
+        }
+    }
 }
