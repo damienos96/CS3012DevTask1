1bcdbb1a8b789aa106500b48a36c41a92b6227df
damienos96 <damiandebny@hotmail.com>
2017-10-31 19:15:36 +0000

    Add DFS to DevTask

--- src/DevTask1.java
+++ src/DevTask1.java
@@ -5,11 +5,13 @@ public class DevTask1<Key extends Comparable<Key>, Value>
 {
 	private final int V;
 	private List<Key>[] adj;
+	private boolean[] marked;
 	
 	public DevTask1(int V)
 	{
 		this.V = V;
-		adj = new List[V];
+		adj = (List<Key>[]) new List[V];
+		marked = new boolean[this.V];
 	}
 	
 	public void addEdge(int v, Key w)
@@ -17,10 +19,25 @@ public class DevTask1<Key extends Comparable<Key>, Value>
 		adj[v].add(w);
 	}
 	
+	public void depthFirstSearch(int v)
+	{
+		marked[v] = true;
+		for(int w = 0; w < adj.length; w++)
+		{
+			if(!marked[w])
+			{
+				depthFirstSearch(w);
+			}
+		}
+	}
+	
+	public boolean visited(int v)
+	{
+		return marked[v];
+	}
+	
 	public Key lowestCommonAncestor(int a)
 	{
 		return null;
 	}
-	
-	
 }
