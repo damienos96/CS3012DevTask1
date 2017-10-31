8bbd4c1bc6dca691753add4616622964ae9a020a
damienos96 <damiandebny@hotmail.com>
2017-10-31 18:29:12 +0000

    Redesigned the graph

--- src/DevTask1Test.java
+++ src/DevTask1Test.java
@@ -5,47 +5,14 @@ import org.junit.Test;
 public class DevTask1Test {
 
 	@Test
-	public void testPut() {
+	public void testInsertNode() {
 		DevTask1<Integer, Integer> bst = new DevTask1<Integer, Integer>();
-		bst.put(7, 7);
-	    bst.put(8, 8);
-	    bst.put(3, 3);
-	    assertEquals("Checking the put() function", true, bst.contains(7));
-	    assertEquals("Checking the put() function", true, bst.contains(3));
-	    assertEquals("Checking the put() function", true, bst.contains(8));
-	}
-	
-	@Test
-	public void testHeight()
-	{
-		DevTask1<Integer, Integer> bst = new DevTask1<Integer, Integer>();
-		assertEquals("Checking the height() function for an empty tree", -1, bst.height());
-		bst.put(7, 7);
-	    bst.put(8, 8);
-	    bst.put(3, 3);
-	    assertEquals("Checking the height() function for a non-empty tree", 1, bst.height());
-	}
-	
-	@Test
-	public void testIsEmpty()
-	{
-		DevTask1<Integer, Integer> bst = new DevTask1<Integer, Integer>();
-		assertEquals("Checking the isEmpty() function for an empty tree", true, bst.isEmpty());
-		bst.put(7, 7);
-	    bst.put(8, 8);
-	    bst.put(3, 3);
-	    assertEquals("Checking the isEmpty() function for a non-empty tree", false, bst.isEmpty());
-	}
-	
-	@Test
-	public void testSize()
-	{
-		DevTask1<Integer, Integer> bst = new DevTask1<Integer, Integer>();
-		assertEquals("Checking the size() function for an empty tree", 0, bst.size());
-		bst.put(7, 7);
-	    bst.put(8, 8);
-	    bst.put(3, 3);
-	    assertEquals("Checking the size() function for a non-empty tree", 3, bst.size());
+		bst.insertNode(7, 7);
+	    bst.insertNode(8, 8);
+	    bst.insertNode(3, 3);
+	    assertEquals("Checking the insertNode() function", true, bst.contains(7));
+	    assertEquals("Checking the insertNode() function", true, bst.contains(3));
+	    assertEquals("Checking the insertNode() function", true, bst.contains(8));
 	}
 	
 	@Test
@@ -53,9 +20,9 @@ public class DevTask1Test {
 	{
 		DevTask1<Integer, Integer> bst = new DevTask1<Integer, Integer>();
 		assertEquals("Checking the contains() function for an empty tree", false, bst.contains(5));
-		bst.put(7, 7);
-	    bst.put(8, 8);
-	    bst.put(3, 3);
+		bst.insertNode(7, 7);
+	    bst.insertNode(8, 8);
+	    bst.insertNode(3, 3);
 	    assertEquals("Checking the contains() function for a non-empty tree", true, bst.contains(7));
 	    assertEquals("Checking the contains() function for a non-empty tree", false, bst.contains(1));
 	    assertEquals("Checking the contains() function for a non-empty tree", true, bst.contains(8));
@@ -66,9 +33,9 @@ public class DevTask1Test {
 	public void testDelete()
 	{
 		DevTask1<Integer, Integer> bst = new DevTask1<Integer, Integer>();
-		bst.put(7, 7);
-	    bst.put(8, 8);
-	    bst.put(3, 3);
+		bst.insertNode(7, 7);
+	    bst.insertNode(8, 8);
+	    bst.insertNode(3, 3);
 	    assertEquals("Checking if key 3 is present", true, bst.contains(3));
 	    bst.delete(3);
 	    assertEquals("Checking if key 3 was deleted", false, bst.contains(3));
@@ -78,12 +45,12 @@ public class DevTask1Test {
 	public void testLowestCommonAncestor()
 	{
 		DevTask1<Integer, Integer> bst = new DevTask1<Integer, Integer>();
-		bst.put(7, 7);
-	    bst.put(8, 8);
-	    bst.put(3, 3);
-	    bst.put(1, 1);
-        bst.put(2, 2);
-        bst.put(6, 6);
+		bst.insertNode(7, 7);
+	    bst.insertNode(8, 8);
+	    bst.insertNode(3, 3);
+	    bst.insertNode(1, 1);
+        bst.insertNode(2, 2);
+        bst.insertNode(6, 6);
         assertEquals("Checking the lowestCommonAncestor of a non-empty tree", 7, (int)bst.lowestCommonAncestor(7, 6));
 	}
 }
