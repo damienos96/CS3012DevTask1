f74d66b0798d643932252408f17b4a6b3c3c207c
damienos96 <damiandebny@hotmail.com>
2017-11-01 23:29:45 +0000

    Update DAG and add test templates

--- src/DevTask1.java
+++ src/DevTask1.java
@@ -1,39 +1,130 @@
-//import java.util.Collection;
-import java.util.List;
+import java.util.ArrayList;
 
-public class DevTask1<Key extends Comparable<Key>, Value>
-{
-	private final int V;
-	private List<Key>[] adj;
-	private boolean[] marked;
+public class DevTask1 <Key extends Comparable<Key>, Value> {
 	
-	public DevTask1(int V)
-	{
-		this.V = V;
-		adj = (List<Key>[]) new List[V];
-		marked = new boolean[this.V];
-	}
-	
-	public void addEdge(int v, Key w)
-	{
-		adj[v].add(w);
-	}
-	
-	public void depthFirstSearch(int v)
-	{
-		marked[v] = true;
-		for(int w = 0; w < adj.length; w++)
+	private class Edge {
+		public int u;
+		public int v;
+		public int w;
+		
+		public Edge(int u, int v, int w)
 		{
-			if(!marked[w])
-			{
-				depthFirstSearch(w);
-			}
+			this.u=u;
+			this.v=v;
+			this.w=w;
 		}
 	}
-	
-	public boolean visited(int v)
+	public ArrayList<Edge>[] adjacencyList=null;
+	public int[] inEdgeCount=null;
+	public int noOfVertices;
+
+	public DevTask1(int noOfVertices)
 	{
-		return marked[v];
+		adjacencyList=(ArrayList<Edge>[])new ArrayList[noOfVertices+1];
+		for(int i=1;i<=noOfVertices;i++)
+		{
+			adjacencyList[i]=new ArrayList<Edge>();
+		}
+		inEdgeCount=new int[noOfVertices+1];
+		this.noOfVertices=noOfVertices;
+	}
+
+	/**
+	 * 
+	 * @param u
+	 * @param v
+	 * @param w
+	 * To add edges to the adjacency list graph
+	 */
+	public void addEdge(int u, int v, int w)
+	{
+		if(adjacencyList[u]==null)
+			adjacencyList[u]=new ArrayList<Edge>();
+		adjacencyList[u].add(new Edge(u, v, w));
+	}
+
+	/**
+	 * 
+	 * @param u
+	 * @param v
+	 * To remove the edge from the graph
+	 */
+	public void removeEdge(int u, int v)
+	{
+		int indexToBeRemoved=-1;
+		ArrayList<Edge> edgeList=adjacencyList[u];
+		for(int i=0;i<adjacencyList[u].size();i++)
+		{
+			Edge e=edgeList.get(i);
+			if(e.u==u&&e.v==v)
+			{
+				indexToBeRemoved=i;
+			}
+		}
+		if(indexToBeRemoved!=-1)
+			edgeList.remove(indexToBeRemoved);
+	}
+
+	/**
+	 * 
+	 * @param u
+	 * @return
+	 * To return the outgoing edges for the given source
+	 */
+	public ArrayList<Edge> getOutEdges(int u)
+	{
+		return adjacencyList[u];
+	}
+
+	/**
+	 * 
+	 * @param u
+	 * @param v
+	 * @return
+	 * To get the weight given the u and v values
+	 * 
+	 */
+	public int getWeight(int u, int v)
+	{
+		ArrayList<Edge> edgeList=adjacencyList[u];
+		int weight=0;
+		for(Edge e : edgeList)
+		{
+			if(e.v==v)
+				weight=e.w;
+		}
+		return weight;
+	}
+
+	/**
+	 * To return the array of adjacency list(Actually the graph)
+	 * @return
+	 */
+	public ArrayList<Edge>[] getListOfAdjacencylist()
+	{
+		return adjacencyList;
+	}
+
+	/**
+	 * To return the count of incoming edges for each vertices in an array
+	 * @return
+	 */
+	public int[] getInEdgesCount()
+	{
+		for(int i=1;i<=noOfVertices;i++)
+		{
+			for(int j=1;j<adjacencyList.length;j++)
+			{
+				ArrayList<Edge> list=adjacencyList[j];
+				for(Edge e : list)
+				{
+					if(e.v==i)
+						inEdgeCount[i]++;
+				}
+			}
+
+		}
+		return inEdgeCount;
 	}
 	
 	public Key lowestCommonAncestor(int a)
--- src/DevTask1Test.java
+++ src/DevTask1Test.java
@@ -5,52 +5,49 @@ import org.junit.Test;
 public class DevTask1Test {
 
 	@Test
-	public void testInsertNode() {
-		DevTask1<Integer, Integer> bst = new DevTask1<Integer, Integer>();
-		bst.insertNode(7, 7);
-	    bst.insertNode(8, 8);
-	    bst.insertNode(3, 3);
-	    assertEquals("Checking the insertNode() function", true, bst.contains(7));
-	    assertEquals("Checking the insertNode() function", true, bst.contains(3));
-	    assertEquals("Checking the insertNode() function", true, bst.contains(8));
+	public void testAddEdge() {
+		DevTask1 dag = new DevTask1(3);
+		dag.addEdge(7, 7, 0);
+	    dag.addEdge(8, 8, 0);
+	    dag.addEdge(3, 3, 0);
+	    /*assertEquals("Checking the addEdge() function", true, dag.contains(7));
+	    assertEquals("Checking the addEdge() function", true, dag.contains(3));
+	    assertEquals("Checking the addEdge() function", true, dag.contains(8));*/
 	}
 	
 	@Test
-	public void testContains()
+	public void testRemoveEdge()
 	{
-		DevTask1<Integer, Integer> bst = new DevTask1<Integer, Integer>();
-		assertEquals("Checking the contains() function for an empty tree", false, bst.contains(5));
-		bst.insertNode(7, 7);
-	    bst.insertNode(8, 8);
-	    bst.insertNode(3, 3);
-	    assertEquals("Checking the contains() function for a non-empty tree", true, bst.contains(7));
-	    assertEquals("Checking the contains() function for a non-empty tree", false, bst.contains(1));
-	    assertEquals("Checking the contains() function for a non-empty tree", true, bst.contains(8));
-	    assertEquals("Checking the contains() function for a non-empty tree", true, bst.contains(3));
+		
 	}
 	
 	@Test
-	public void testDelete()
+	public void testGetOutEdge()
 	{
-		DevTask1<Integer, Integer> bst = new DevTask1<Integer, Integer>();
-		bst.insertNode(7, 7);
-	    bst.insertNode(8, 8);
-	    bst.insertNode(3, 3);
-	    assertEquals("Checking if key 3 is present", true, bst.contains(3));
-	    bst.delete(3);
-	    assertEquals("Checking if key 3 was deleted", false, bst.contains(3));
+		
+	}
+	
+	@Test
+	public void testGetWeight()
+	{
+		
+	}
+	
+	@Test
+	public void testGetListOfAdjacencyList()
+	{
+		
+	}
+	
+	@Test
+	public void testGetInEdgesCount()
+	{
+		
 	}
 	
 	@Test
 	public void testLowestCommonAncestor()
 	{
-		DevTask1<Integer, Integer> bst = new DevTask1<Integer, Integer>();
-		bst.insertNode(7, 7);
-	    bst.insertNode(8, 8);
-	    bst.insertNode(3, 3);
-	    bst.insertNode(1, 1);
-        bst.insertNode(2, 2);
-        bst.insertNode(6, 6);
-        assertEquals("Checking the lowestCommonAncestor of a non-empty tree", 7, (int)bst.lowestCommonAncestor(7, 6));
+		
 	}
 }
