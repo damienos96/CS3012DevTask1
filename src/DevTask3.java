79afca9b65ce6fc3da21b9f641115c98f0a32c53
damienos96 <damiandebny@hotmail.com>
2017-12-14 15:10:10 +0000

    Trying to access github data

--- src/DevTask3.java
+++ src/DevTask3.java
@@ -1,9 +1,53 @@
 
+//package com.github.kevinsawicki.api;
+
+import java.io.IOException;
+import java.text.MessageFormat;
+import java.util.Collection;
+import java.util.Date;
+
+import org.eclipse.egit.github.core.RepositoryCommit;
+import org.eclipse.egit.github.core.RepositoryId;
+import org.eclipse.egit.github.core.service.CommitService;
+import org.eclipse.egit.github.core.Repository;
+import org.eclipse.egit.github.core.service.RepositoryService;
+
 public class DevTask3 {
 
-	public static void main(String[] args) {
-		
-
+	public static void main(String[] args) throws IOException{
+		//PageCommits();
+		PrintRepos();
 	}
-
+	
+	
+	public static void PageCommits()
+	{
+		final int size = 25;
+		final RepositoryId repo = new RepositoryId("github", "hubot");
+		final String message = "   {0} by {1} on {2}";
+		final CommitService service = new CommitService();
+		int pages = 1;
+		for (Collection<RepositoryCommit> commits : service.pageCommits(repo,
+				size)) {
+			System.out.println("Commit Page " + pages++);
+			for (RepositoryCommit commit : commits) {
+				String sha = commit.getSha().substring(0, 7);
+				String author = commit.getCommit().getAuthor().getName();
+				Date date = commit.getCommit().getAuthor().getDate();
+				System.out.println(MessageFormat.format(message, sha, author,
+						date));
+			}
+		}
+	}
+	
+	public static void PrintRepos() throws IOException
+	{
+		final String user = "defunkt";
+		final String format = "{0}) {1}- created on {2}";
+		int count = 1;
+		RepositoryService service = new RepositoryService();
+		for (Repository repo : service.getRepositories(user))
+			System.out.println(MessageFormat.format(format, count++,
+					repo.getName(), repo.getCreatedAt()));
+	}
 }
