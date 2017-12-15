7335be36e7da8a8443355e896e815568d93f5d0f
damienos96 <damiandebny@hotmail.com>
2017-12-15 18:59:56 +0000

    Improved pageCommits()

--- src/DevTask3.java
+++ src/DevTask3.java
@@ -1,5 +1,5 @@
-
-//package com.github.kevinsawicki.api;
+//Author: Damian Debny
+//Student No. 15315535
 
 import java.io.IOException;
 import java.text.MessageFormat;
@@ -20,48 +20,47 @@ public class DevTask3 {
 		System.out.println("Enter the name of a Github user: ");
 		Scanner input = new Scanner(System.in);
 		String user = input.nextLine();
-		
 		printRepos(user);
-		pageCommits(user);
+		System.out.println("Enter the name of one of the given repositories: ");
+		String repo = input.nextLine();
+		pageCommits(user, repo);
 		
 		//printWatchers();
-		//printLanguages();
-		
 		input.close();
 	}
 	
 	
-	public static void pageCommits(String user)
+	public static void pageCommits(String user, String rep)
 	{
 		System.out.println("Page commits for " + user);
 		final int size = 25;
-		RepositoryId repo = new RepositoryId(user, "githubAPI");
+		RepositoryId repo = new RepositoryId(user, rep);
 		final String message = "   {0} by {1} on {2}";
 		final CommitService service = new CommitService();
 		int pages = 1;
+		int totalCommits = 0;
 		for (Collection<RepositoryCommit> commits : service.pageCommits(repo,size))
 		{
+			totalCommits += commits.size();
 			System.out.println("Commit Page " + pages++);
 			for (RepositoryCommit commit : commits) {
 				String sha = commit.getSha().substring(0, 7);
 				String author = commit.getCommit().getAuthor().getName();
 				Date date = commit.getCommit().getAuthor().getDate();
-				System.out.println(MessageFormat.format(message, sha, author,
-						date));
+				System.out.println(MessageFormat.format(message, sha, author, date));
 			}
 		}
+		System.out.println("Total number of commits " + totalCommits);
 	}
 	
 	public static void printRepos(String user) throws IOException
 	{
-		//final String user = owner;
 		System.out.println(user + "'s Repositories:");
 		final String format = "{0}) {1}- created on {2}, language used: {3}";
 		int count = 1;
 		RepositoryService service = new RepositoryService();
 		for (Repository repo : service.getRepositories(user))
-			System.out.println(MessageFormat.format(format, count++,
-					repo.getName(), repo.getCreatedAt(), repo.getLanguage()));
+			System.out.println(MessageFormat.format(format, count++, repo.getName(), repo.getCreatedAt(), repo.getLanguage()));
 	}
 	
 	public static void printWatchers() throws IOException
